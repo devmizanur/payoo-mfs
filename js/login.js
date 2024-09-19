33c18fb63d0a77e8631385f4fbed8af8c259bf38
devmizanur <sohag.bogra420@gmail.com>
2024-09-19 07:34:13 +0600

    added event handerls

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-login').addEventListener('click',function(event){
+event.preventDefault();//for must be use in form it prevent reloding the page
+// step-2
+const phoneNumber=document.getElementById('phone-number').value;
+
+})
