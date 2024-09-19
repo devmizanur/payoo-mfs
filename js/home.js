ce0d27f35f3862b9c6123e5ff67788e9407ba34e
devmizanur <sohag.bogra420@gmail.com>
2024-09-19 17:59:13 +0600

    added cash out option

--- js/home.js
+++ js/home.js
@@ -14,3 +14,21 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
  }
 
 })
+/**
+ * cash out js-start
+ */
+document.getElementById('btn-cash-out').addEventListener('click',function(event){
+  event.preventDefault();
+  const cashOut=document.getElementById('input-cash-out').value;
+  const cashOutPin=document.getElementById('input-cash-out-pin').value;
+  if (cashOutPin==='12345') {
+    const startBalance=document.getElementById('account-balance').innerText;
+    const cashOutInput=parseFloat(cashOut);
+    const presentBalance=parseFloat(startBalance);
+    const newAfterOut= presentBalance -cashOutInput;
+    document.getElementById('account-balance').innerText=newAfterOut;
+  } else {
+    alert('cash out failed!');
+  }
+
+})
