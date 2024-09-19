75ebfc94a758a243b34597abf3f3b9835012d6f6
devmizanur <sohag.bogra420@gmail.com>
2024-09-19 19:15:05 +0600

    fix directory issue

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ event.preventDefault();//for must be use in form it prevent reloding the page
 const phoneNumber=document.getElementById('phone-number').value;
 const pinNumber=document.getElementById('pin-number').value;
 if (phoneNumber==='01751551434' && pinNumber==='12345') {
-  window.location.href="/home.html";//redirect to another page
+  window.location.href="home.html";//redirect to another page
 
 } else {
   alert('wrong phone number or pin');
