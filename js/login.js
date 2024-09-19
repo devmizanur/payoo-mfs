58b5d52be4872aa0d5aa83772cd8a7eaaf81ab90
devmizanur <sohag.bogra420@gmail.com>
2024-09-19 09:25:33 +0600

    added function

--- js/login.js
+++ js/login.js
@@ -2,5 +2,12 @@ document.getElementById('btn-login').addEventListener('click',function(event){
 event.preventDefault();//for must be use in form it prevent reloding the page
 // step-2
 const phoneNumber=document.getElementById('phone-number').value;
+const pinNumber=document.getElementById('pin-number').value;
+if (phoneNumber==='01751551434' && pinNumber==='12345') {
+  window.location.href="/home.html";//redirect to another page
+
+} else {
+  alert('wrong phone number or pin');
+}
 
 })
